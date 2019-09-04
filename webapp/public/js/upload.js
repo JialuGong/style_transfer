c31920a3fcf42110579e942cd657bec384e43adb
Huaiyu Gong <HuaiyuGong123@gmail.com>
2019-09-04 14:22:38 +0800

    add index.jsp show.jsp uploadTest.jsp and some js files for more to read readme.md

--- webapp/public/js/upload.js
+++ webapp/public/js/upload.js
@@ -1,79 +1,93 @@
-function file1Selected() {
-    var file = document.getElementById('fileToUpload_1').files[0];
-    if (file) {
-        //显示选择的本地图片
-        var url=window.URL.createObjectURL(file);
-       // alert(url.toString());
-       //document.getElementById('source_img_1').append('<img src="'+url+'"/>');
-        document.getElementById('source_img_1').src=url;
-
-       // var reader=new FileReader();
-        //reader.onload=function(evt){
-         //   document.getElementById('source_img_1').src=evt.target.result;
-        //    source_img=evt.target.result;
-        //};
-        //reader.readAsDataURL(file.files[0]);
-        //控制图片的大小和格式
-        var fileSize=file.size;
+//检验图片格式是否正确的函数
+function examine(id){
+    var file=document.getElementById(id).files[0];
+    var pattern=/image\W/;
+    if(file){
         var fileType=file.type;
-        alert(fileType);
-        if(fileType !== 'image/'){
-            alert("请选择正确的图片格式!")
-        }else{
-            if(fileSize>2*1024*1024){
-                alert("图片大小不能超过2MB!");
+        var fileSize=file.size;
+        if(pattern.test(fileType)){
+            if(fileSize<=2*1024*1024){
+                return true;
+            }
+            else{
+                alert("图片的大小不能超过2M");
+                document.getElementById(id).value="";
+                return false;
             }
         }
-        if (file.size > 1024 * 1024)
-            fileSize_string = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-        else
-            fileSize_string = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
-        document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
-        document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize_string;
-    }
-}
-
-function file2Selected() {
-    var file = document.getElementById('fileToUpload_2').files[0];
-    if (file) {
-        //显示图片
-        var url = window.URL.createObjectURL(file);
-        document.getElementById('source_img_2').src = url;
-        //显示图片信息
-        var fileSize = 0;
-        if (file.size > 1024 * 1024)
-            fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-        else
-            fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
-        document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
-        document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
-    }
-}
-
-function uploadFile() {
-    var fd = new FormData();
-    var file1=document.getElementById('fileToUpload_1').files[0];
-    var file2=document.getElementById('fileToUpload_2').files[0];
-    if(file1.name===file2.name){
-        alert("两张图片不能相同,请重新选择")
-    }
-    else if(!file1 || !file2){
-        alert("请选择两张图片")
+        else{
+            alert("请选择正确的图片格式");
+            document.getElementById(id).value="";
+            return false;
+        }
     }
     else{
-        fd.append("fileToUpload_1",file1);
-        fd.append("fileToUpload_2",file2);
-        var xhr = new XMLHttpRequest();
-        xhr.upload.addEventListener("progress", uploadProgress, false);
-        xhr.addEventListener("load", uploadComplete, false);
-        xhr.addEventListener("error", uploadFailed, false);
-        xhr.addEventListener("abort", uploadCanceled, false);
-        xhr.open("POST", "upload.do"); //修改成自己的接口
-        xhr.send(fd);
+        alert("请选择图片");
+        return false;
     }
-
+}
+//图片一的显示
+function file1Selected(){
+    if(examine("fileToUpload_1")){
+        var file=document.getElementById('fileToUpload_1').files[0];
+        var url = window.URL.createObjectURL(file);
+        document.getElementById('source_img_1').src = url;
+    }
+}
+//图片二的显示
+function file2Selected(){
+    if(examine("fileToUpload_2")){
+        var file=document.getElementById('fileToUpload_2').files[0];
+        var url = window.URL.createObjectURL(file);
+        document.getElementById('source_img_2').src=url;
+    }
 }
 
+//图片的上传
+function uploadFile(){
+	var basePath = document.getElementsByTagName('base')[0].href;
+    id1="fileToUpload_1";
+    id2="fileToUpload_2";
+    //检查图片路径是否相同,以此判断图片是否相同
+    if(examine(id1)&&examine(id2)){
+        var file1=document.getElementById(id1).files[0];
+        var file2=document.getElementById(id2).files[0];
+        var url1=window.URL.createObjectURL(file1);
+        var url2=window.URL.createObjectURL(file2);
+        if(url1===url2){
+            alert("请选择两张不同的图片");
+            file1.value="";
+            file2.value="";
+        }
+        else{
+            var formdata=new FormData();
+            formdata.append("file1",file1);
+            formdata.append("file2",file2);
+            if(!file1 || !file2){
+            	alert("error");
+            }
+            //使用ajax上传图片
+            $.ajax({
+                    type:"POST",
+                    url : basePath+'test/userPhotoUpload',
+                    mimeType : 'multipart/form-data',
+                    data:formdata,
+                    contentType:false,
+                    processData:false,
+                    success:function(data){
+
+                        console.log(data);
+                    },
+                    error:function(){
+                        alert("图片上传错误,请重现上传");
+                    }
+                }
+            );
+        }
+    }
+}
+
+//显示图片上传的进度
 function uploadProgress(evt) {
     if (evt.lengthComputable) {
         var percentComplete = Math.round(evt.loaded * 100 / evt.total);
@@ -82,40 +96,11 @@ function uploadProgress(evt) {
         document.getElementById('progressNumber').innerHTML = 'unable to compute';
     }
 }
-
+/* 服务器端返回响应时候触发event事件*/
 function uploadComplete(evt) {
-    /* 服务器端返回响应时候触发event事件*/
     alert(evt.target.responseText);
 }
-
+//图片上传的错误提示
 function uploadFailed(evt) {
     alert("There was an error attempting to upload the file.");
-}
-
-function uploadCanceled(evt) {
-    alert("The upload has been canceled by the user or the browser dropped the connection.");
-}
-
-function getFile(){
-    var xhr=new XMLHttpRequest();
-    //url 未填
-    xhr.open("GET","47.92.141.59/test/png/test.png",true);
-
-    xhr.responseType="blob";
-
-    xhr.onload=function(){
-        if(this.status===200){
-            alert("success!");
-            var blob=this.response;
-            var img=document.createElement("img");
-            img.onload=function(e) {
-                window.URL.revokeObjectURL(img.src);
-                document.body.appendChild(img);
-            }
-        }
-        else {
-            alert("error!");
-        }
-    };
-    xhr.send();
 }
